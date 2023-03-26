c199bf32ce5bac73376d58c03542fc17a3179dd0
linkhantko <linkhantko1@gmail.com>
2023-03-27 00:26:34 +0630

    lkk

---
+++ app/Http/Controllers/API/PostController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return response()->json($posts, 200 );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $post = Post::create([
+            'title' => $request->title,
+            'description' => $request->description
+        ]);
+        return response()->json($post, 200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $post = Post::find($id);
+        return response()->json($post, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $post = Post::findOrFail($id);
+        $post->update([
+            'title' => $request->title,
+            'description' => $request->description
+        ]);
+        return response()->json(['mes' => "Update Successful"], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Post::findOrFail($id)->delete();
+        return response()->json(['mes' => 'Delete Success'], 200);
+    }
+}
